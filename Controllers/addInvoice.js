146aaa8caccb8f69f77132afe0f5701fd25d9f85
Mohamed Tarek <mohamedtarek.4500@gmail>
2024-01-06 16:46:33 +0200

    send invoice email

--- Controllers/addInvoice.js
+++ Controllers/addInvoice.js
@@ -1,9 +1,11 @@
 const Invoice = require('../Models/invoice')
 const Client = require('../Models/client')
+const {sendInvoiceEmail} = require('../Email Sender/emailController')
 
 module.exports = (req , res ,next)=>{
     const clientID = req.body.clientID
     const userID = req.user._id
+    const userName = req.user.firstName + " " + req.user.lastName
     
     Invoice.create(req.body.invoice)
     .then(invoice =>{
@@ -14,11 +16,18 @@ module.exports = (req , res ,next)=>{
                 invoiceID : invoice._id
             }
 
+            const invoiceEmailObj = {
+                clientEmail : client.email,
+                userName : userName,
+                invoiceDetails : invoice
+            }
+
             client.invoices.push(invoiceObj)
             client.save()
             .then(client =>{
                 res.status = 200
                 res.end()
+                sendInvoiceEmail(invoiceEmailObj)
             })
         })
     })
