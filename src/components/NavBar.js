65d9e2b68c425688469e2e729947f6c37abfd19c
Marwan Elgendy <marwantarek852@gmail.com>
2024-01-09 22:55:35 +0300

    call userLogout api to logout the user

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -14,14 +14,18 @@ import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 import { ImageListItem } from "@mui/material";
 
-import Logo from '../images/logo.png'
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import Logo from "../images/logo.png";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const settings = ["Profile", "Dashboard", "Logout"];
 
 export const NavBar = () => {
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const navigate = useNavigate()
 
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
@@ -31,12 +35,23 @@ export const NavBar = () => {
     setAnchorElUser(null);
   };
 
+  const handleLogout = ()=>{
+    axios.post("http://localhost:5000/userRegisteration/logout",{}, {withCredentials : true})
+    .then(response =>{
+      navigate('/')
+    })
+    .catch(error => console.log({error}))
+  }
+
   return (
-    <AppBar position="static" sx={{position:'relative' , zIndex: (theme) => theme.zIndex.drawer + 1}}>
-      <Container maxWidth="xl" sx={{paddingY : '8px'}}>
-        <Toolbar disableGutters sx={{justifyContent: 'space-between'}}>
+    <AppBar
+      position="static"
+      sx={{ position: "relative", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+    >
+      <Container maxWidth="xl" sx={{ paddingY: "8px" }}>
+        <Toolbar disableGutters sx={{ justifyContent: "space-between" }}>
           {/* Logo for large Screen */}
-          <ImageListItem sx={{width : '5rem'}}>
+          <ImageListItem sx={{ width: "5rem" }}>
             <img src={Logo} alt="" />
           </ImageListItem>
 
@@ -44,7 +59,9 @@ export const NavBar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <AccountCircleOutlinedIcon sx={{fontSize : '2.5rem' , color: 'white'}} />
+                <AccountCircleOutlinedIcon
+                  sx={{ fontSize: "2.5rem", color: "white" }}
+                />
               </IconButton>
             </Tooltip>
             <Menu
@@ -63,11 +80,17 @@ export const NavBar = () => {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
+              {settings.map((setting) =>
+                setting == "Logout" ? (
+                  <MenuItem key={setting} onClick={handleLogout}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ) : (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                )
+              )}
             </Menu>
           </Box>
         </Toolbar>
